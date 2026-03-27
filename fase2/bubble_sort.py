19295368547314375374cd86d2cedac1f430bf72
iconoclasst <davi09bezerra70@gmail.com>
2026-03-27 16:17:00 -0300

    automatic commit

--- fase2/bubble_sort.py
+++ fase2/bubble_sort.py
@@ -10,12 +10,23 @@ def bs(array):
                     array[i] = array[i+1]
                     array[i+1] = aux
 
+# versão 2
+def bs_v2(array):
+    n = len(array)
+    for j in range(n):
+        for i in range(n - j - 1):
+            if array[i] < array[i+1]:
+                array[i], array[i+1] = array[i+1], array[i]
 
-n = int(input("Insira N: "))
-array=[]
-for i in range(n):
-    array.append(int(input("Insira um valor: ")))
+#n = int(input("Insira N: "))
+#array=[]
+#for i in range(n):
+ #   array.append(int(input("Insira um valor: ")))
 
-print(array)
-bs(array)
-print(array)
+#print(array)
+#bs(array)
+#print(array)
+
+a = [3,5,1,2,3,3,3,7,5,-2,5]
+bs_v2(a)
+print(a)
