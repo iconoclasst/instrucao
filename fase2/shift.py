c8a7f5d89239b744b7d888e3fbbe1d1400f20940
iconoclasst <davi09bezerra70@gmail.com>
2026-03-30 20:16:49 -0300

    automatic commit

--- fase2/shift.py
+++ fase2/shift.py
@@ -1,20 +1,21 @@
 # Dado um vetor e um numero k, fazer uma rotação em k vezes.
 
 def shift(array, k):
-    # n = len(array)
-    pass
-    
-
+    n = len(array)
+    aux = 0
+    for i in range(n):
+        if i+k > n:
+            
+            aux[i] = array[(i+k)%n]
+    return aux
 
 n = int(input("Insira N: "))
 
 array = [int(input("Insira um valor: ")) for i in range(n)]
 k = int(input("Insira o tamanho do shift: "))
 
-for i in range(n):
-    if array[i-k] < 0:
-        i = n - k
-    array[i-k] = array[i]
-    
-
 print(array)
+
+aux = shift(array, k)
+
+print(aux)
